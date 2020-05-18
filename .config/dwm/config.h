8f3f20ab1144bf5dd49b7cffb54e05ad641d815e
Ryan Blachowicz <ryansblachowicz@gmail.com>
2020-05-18 15:06:49 -0400

    mv and cp alias

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -38,7 +38,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ NULL,      "feh",       NULL,       1 << 8,       1,           -1 },
+	{ NULL,      "feh",       NULL,       1,            1,           -1 },
 };
 
 /* layout(s) */
